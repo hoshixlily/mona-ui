8fe3552456a7f8e3e050edc2b83fe8d6ec540f3c
Nanahoshi <nanahoshii@outlook.com>
2022-08-18 16:40:52 +0300

    Improvements to slider dragging

--- projects/mona-ui/src/lib/inputs/modules/slider/components/slider/slider.component.ts
+++ projects/mona-ui/src/lib/inputs/modules/slider/components/slider/slider.component.ts
@@ -1,26 +1,48 @@
-import { Component, ElementRef, NgZone, OnInit } from "@angular/core";
+import {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    ElementRef,
+    NgZone,
+    OnInit,
+    Renderer2
+} from "@angular/core";
 import { fromEvent } from "rxjs";
 
 @Component({
     selector: "mona-slider",
     templateUrl: "./slider.component.html",
-    styleUrls: ["./slider.component.scss"]
+    styleUrls: ["./slider.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SliderComponent implements OnInit {
-    private dragging: boolean = false;
+export class SliderComponent implements OnInit, AfterViewInit {
+    public dragging: boolean = false;
     public handlerLeftPosition: number = 0;
 
-    public constructor(private readonly elementRef: ElementRef<HTMLElement>, private readonly zone: NgZone) {}
+    public constructor(
+        private readonly elementRef: ElementRef<HTMLElement>,
+        private renderer: Renderer2,
+        private readonly cdr: ChangeDetectorRef,
+        private readonly zone: NgZone
+    ) {}
 
-    public ngOnInit(): void {
+    // public ngAfterContentChecked(): void {
+    //     console.log("ngAfterContentChecked");
+    // }
+
+    public ngAfterViewInit(): void {
         this.setEventListeners();
     }
+
+    public ngOnInit(): void {}
 
     public onHandlerMouseDown(): void {
         if (this.dragging) {
             return;
         }
         this.dragging = true;
+        document.documentElement.style.userSelect = "none";
     }
 
     public onTickClick(
@@ -29,62 +51,75 @@ export class SliderComponent implements OnInit {
         sliderTrackElement: HTMLDivElement,
         sliderHandlerElement: HTMLDivElement
     ): void {
-        this.setHandlerPosition(event, tickElement, sliderTrackElement, sliderHandlerElement);
+        this.handlerLeftPosition = this.findHandlerPosition(
+            event,
+            tickElement,
+            sliderTrackElement,
+            sliderHandlerElement
+        );
     }
 
-    public onTickMouseMove(
-        event: MouseEvent,
-        tickElement: HTMLSpanElement,
-        sliderTrackElement: HTMLDivElement,
-        sliderHandlerElement: HTMLDivElement
-    ): void {
-        if (!this.dragging) {
-            return;
+    private findClosestTickElement(event: MouseEvent): HTMLSpanElement {
+        const elements = Array.from(
+            this.elementRef.nativeElement.querySelectorAll(".mona-slider-tick > span")
+        ) as HTMLSpanElement[];
+        let maxDistance = Number.MAX_VALUE;
+        let index = 0;
+        for (const [ex, element] of elements.entries()) {
+            const rect = element.getBoundingClientRect();
+            const distance = Math.sqrt(Math.pow(rect.left - event.clientX, 2) + Math.pow(rect.top - event.clientY, 2));
+            if (distance < maxDistance) {
+                maxDistance = distance;
+                index = ex;
+            }
         }
-        this.setHandlerPosition(event, tickElement, sliderTrackElement, sliderHandlerElement);
+        return elements[index];
     }
 
-    private setHandlerPosition(
+    private findHandlerPosition(
         event: MouseEvent,
         tickElement: HTMLSpanElement,
         sliderTrackElement: HTMLDivElement,
         sliderHandlerElement: HTMLDivElement
-    ): void {
+    ): number {
         const rect = tickElement.getBoundingClientRect();
         const containerRect = sliderTrackElement.getBoundingClientRect();
         const handlerRect = sliderHandlerElement.getBoundingClientRect();
 
-        const distanceToLeft = event.clientX - rect.left;
-        const distanceToRight = rect.right - event.clientX;
+        const distanceToLeft = Math.abs(rect.left - event.clientX);
+        const distanceToRight = Math.abs(rect.right - event.clientX);
 
         if (distanceToLeft > distanceToRight) {
-            this.handlerLeftPosition = rect.left - containerRect.left + handlerRect.width / 2;
+            return rect.right - containerRect.left - handlerRect.width / 2;
         } else {
             const siblingRect = tickElement.previousElementSibling?.getBoundingClientRect();
             if (siblingRect) {
-                this.handlerLeftPosition = siblingRect.left - containerRect.left + handlerRect.width / 2;
+                return siblingRect.right - containerRect.left - handlerRect.width / 2;
             } else {
-                this.handlerLeftPosition = (-1 * handlerRect.width) / 2;
+                return (-1 * handlerRect.width) / 2;
             }
         }
     }
 
     private setEventListeners(): void {
         this.zone.runOutsideAngular(() => {
-            const elements = document.querySelectorAll(".mona-slider-tick > span");
-            fromEvent(elements, "mousemove", (event: MouseEvent) => {
+            this.renderer.listen(document, "mousemove", (event: MouseEvent) => {
                 if (!this.dragging) {
                     return;
                 }
-                const tickElement = event.target as HTMLSpanElement;
-                this.zone.run(() => {
-                    this.setHandlerPosition(
-                        event,
-                        tickElement.parentElement as HTMLSpanElement,
-                        this.elementRef.nativeElement.querySelector(".mona-slider-track") as HTMLDivElement,
-                        this.elementRef.nativeElement.querySelector(".mona-slider-handler") as HTMLDivElement
-                    );
-                });
+                const tickElement = this.findClosestTickElement(event);
+                const position = this.findHandlerPosition(
+                    event,
+                    tickElement.parentElement as HTMLSpanElement,
+                    this.elementRef.nativeElement.querySelector(".mona-slider-track") as HTMLDivElement,
+                    this.elementRef.nativeElement.querySelector(".mona-slider-handler") as HTMLDivElement
+                );
+                if (position !== this.handlerLeftPosition) {
+                    this.zone.run(() => {
+                        this.handlerLeftPosition = position;
+                        this.cdr.detectChanges();
+                    });
+                }
             });
             fromEvent(document, "mouseup").subscribe(() => {
                 if (!this.dragging) {
@@ -92,6 +127,8 @@ export class SliderComponent implements OnInit {
                 }
                 this.zone.run(() => {
                     this.dragging = false;
+                    document.documentElement.style.removeProperty("user-select");
+                    console.log("dragging", this.dragging);
                 });
             });
         });
