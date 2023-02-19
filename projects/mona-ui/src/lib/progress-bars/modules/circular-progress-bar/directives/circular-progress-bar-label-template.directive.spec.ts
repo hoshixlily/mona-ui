d26f8a373754917b4447b966864f95b68e5c996d
Nanahoshi <nanahoshii@outlook.com>
2023-02-19 19:11:13 +0300

    Circular progress bar component

---
+++ projects/mona-ui/src/lib/progress-bars/modules/circular-progress-bar/directives/circular-progress-bar-label-template.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CircularProgressBarLabelTemplateDirective } from './circular-progress-bar-label-template.directive';
+
+describe('CircularProgressBarLabelTemplateDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CircularProgressBarLabelTemplateDirective();
+    expect(directive).toBeTruthy();
+  });
+});
