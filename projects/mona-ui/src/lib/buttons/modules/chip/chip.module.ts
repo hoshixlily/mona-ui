1a2159a82a910cc76bd836a5a009b7c7f91bac99
Nanahoshi <nanahoshii@outlook.com>
2023-05-27 23:12:35 +0300

    fix: use padding instead of strict height values

--- projects/mona-ui/src/lib/buttons/modules/chip/chip.module.ts
+++ projects/mona-ui/src/lib/buttons/modules/chip/chip.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ChipComponent } from "./components/chip/chip.component";
+import {ButtonModule} from "../button/button.module";
 
 @NgModule({
     declarations: [ChipComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, ButtonModule],
     exports: [ChipComponent]
 })
 export class ChipModule {}
