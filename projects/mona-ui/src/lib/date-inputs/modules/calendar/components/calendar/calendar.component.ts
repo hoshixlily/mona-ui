ab928d5a5b675e65084e8da4c5165421a8d25c97
Nanahoshi <nanahoshii@outlook.com>
2023-07-14 19:09:46 +0300

    refactor: calendar

--- projects/mona-ui/src/lib/date-inputs/modules/calendar/components/calendar/calendar.component.ts
+++ projects/mona-ui/src/lib/date-inputs/modules/calendar/components/calendar/calendar.component.ts
@@ -1,10 +1,10 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, forwardRef, OnInit } from "@angular/core";
-import { DateTime } from "luxon";
+import { ChangeDetectionStrategy, Component, forwardRef, Input, OnInit } from "@angular/core";
+import { DateTime, DurationObjectUnits } from "luxon";
 import { CalendarView } from "../../../../models/CalendarView";
 import { Dictionary } from "@mirei/ts-collections";
 import { faChevronLeft, faChevronRight, IconDefinition } from "@fortawesome/free-solid-svg-icons";
-import { AbstractDateInputComponent } from "../../../../components/abstract-date-input/abstract-date-input.component";
-import { NG_VALUE_ACCESSOR } from "@angular/forms";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+import { Action } from "../../../../../utils/Action";
 
 @Component({
     selector: "mona-calendar",
@@ -19,20 +19,37 @@ import { NG_VALUE_ACCESSOR } from "@angular/forms";
         }
     ]
 })
-export class CalendarComponent extends AbstractDateInputComponent implements OnInit {
+export class CalendarComponent implements OnInit, ControlValueAccessor {
+    #propagateChange: Action<Date | null> | null = null;
+    #value: Date | null = null;
     public readonly nextMonthIcon: IconDefinition = faChevronRight;
     public readonly prevMonthIcon: IconDefinition = faChevronLeft;
     public calendarView: CalendarView = "month";
+    public currentDateString: string = "";
     public decadeYears: number[] = [];
     public monthBounds: { start: Date; end: Date } = { start: new Date(), end: new Date() };
     public monthlyViewDict: Dictionary<Date, number> = new Dictionary<Date, number>();
+    public navigatedDate: Date = new Date();
 
-    public constructor(protected override readonly cdr: ChangeDetectorRef) {
-        super(cdr);
-    }
+    @Input()
+    public disabled: boolean = false;
 
-    public override ngOnInit(): void {
-        super.ngOnInit();
+    @Input()
+    public disabledDates: Iterable<Date> = [];
+
+    @Input()
+    public format: string = "HH:mm";
+
+    @Input()
+    public max: Date | null = null;
+
+    @Input()
+    public min: Date | null = null;
+
+    public constructor() {}
+
+    public ngOnInit(): void {
+        this.setDateValues();
         const date = this.value ?? DateTime.now().toJSDate();
         this.prepareMonthlyViewDictionary(date);
         this.navigatedDate = date;
@@ -55,7 +72,6 @@ export class CalendarComponent extends AbstractDateInputComponent implements OnI
             this.navigatedDate = date;
             this.prepareMonthlyViewDictionary(date);
         }
-        this.cdr.markForCheck();
     }
 
     public onMonthClick(month: number): void {
@@ -65,22 +81,27 @@ export class CalendarComponent extends AbstractDateInputComponent implements OnI
     }
 
     public onNavigationClick(direction: "prev" | "next"): void {
+        const date = DateTime.fromJSDate(this.navigatedDate);
+        let unit: DurationObjectUnits;
+
+        switch (this.calendarView) {
+            case "month":
+                unit = { months: 1 };
+                break;
+            case "year":
+                unit = { years: 1 };
+                break;
+            case "decade":
+                unit = { years: 10 };
+                break;
+        }
+
+        this.navigatedDate = direction === "prev" ? date.minus(unit).toJSDate() : date.plus(unit).toJSDate();
         if (this.calendarView === "month") {
-            const date = DateTime.fromJSDate(this.navigatedDate);
-            this.navigatedDate =
-                direction === "prev" ? date.minus({ months: 1 }).toJSDate() : date.plus({ months: 1 }).toJSDate();
             this.prepareMonthlyViewDictionary(this.navigatedDate);
-        } else if (this.calendarView === "year") {
-            const date = DateTime.fromJSDate(this.navigatedDate);
-            this.navigatedDate =
-                direction === "prev" ? date.minus({ years: 1 }).toJSDate() : date.plus({ years: 1 }).toJSDate();
         } else if (this.calendarView === "decade") {
-            const date = DateTime.fromJSDate(this.navigatedDate);
-            this.navigatedDate =
-                direction === "prev" ? date.minus({ years: 10 }).toJSDate() : date.plus({ years: 10 }).toJSDate();
             this.prepareDecadeYears();
         }
-        this.cdr.markForCheck();
     }
 
     public onViewChangeClick(view: CalendarView): void {
@@ -93,6 +114,26 @@ export class CalendarComponent extends AbstractDateInputComponent implements OnI
     public onYearClick(year: number): void {
         this.navigatedDate = DateTime.fromJSDate(this.navigatedDate).set({ year }).toJSDate();
         this.calendarView = "year";
+    }
+
+    public registerOnChange(fn: any): void {
+        this.#propagateChange = fn;
+    }
+
+    public registerOnTouched(fn: any): void {}
+
+    public setDisabledState(isDisabled: boolean): void {
+        this.disabled = isDisabled;
+    }
+
+    public writeValue(date: Date | null | undefined): void {
+        this.#value = date ?? null;
+        if (date == null) {
+            this.currentDateString = "";
+        } else {
+            this.currentDateString = DateTime.fromJSDate(date).toFormat(this.format);
+        }
+        this.setDateValues();
     }
 
     private prepareDecadeYears(): void {
@@ -118,10 +159,30 @@ export class CalendarComponent extends AbstractDateInputComponent implements OnI
         }
         this.monthlyViewDict = dictionary;
         this.monthBounds = { start: firstDayOfMonth.toJSDate(), end: lastDayOfMonth.toJSDate() };
-        this.cdr.markForCheck();
+    }
+
+    private setCurrentDate(date: Date | null): void {
+        this.#value = date;
+        if (date) {
+            this.currentDateString = DateTime.fromJSDate(date).toFormat(this.format);
+        } else {
+            this.currentDateString = "";
+        }
+        this.#propagateChange?.(date);
+    }
+
+    private setDateValues(): void {
+        this.navigatedDate = this.value ?? DateTime.now().toJSDate();
+        if (this.value) {
+            this.currentDateString = DateTime.fromJSDate(this.value).toFormat(this.format);
+        }
     }
 
     public get timezone(): string {
         return DateTime.local().zoneName;
+    }
+
+    public get value(): Date | null {
+        return this.#value;
     }
 }
