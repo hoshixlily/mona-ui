a514b02f4fb37c01ac7cfc8041b2fc1aa107fe64
Nanahoshi <nanahoshii@outlook.com>
2023-07-15 02:58:58 +0300

    refactor: date picker

--- projects/mona-ui/src/lib/date-inputs/modules/date-picker/components/date-picker/date-picker.component.ts
+++ projects/mona-ui/src/lib/date-inputs/modules/date-picker/components/date-picker/date-picker.component.ts
@@ -4,13 +4,21 @@ import {
     Component,
     ElementRef,
     forwardRef,
+    HostBinding,
     Input,
-    OnInit
+    OnInit,
+    TemplateRef,
+    ViewChild
 } from "@angular/core";
 import { PopupService } from "../../../../../popup/services/popup.service";
 import { FocusMonitor } from "@angular/cdk/a11y";
-import { AbstractDatePickerComponent } from "../../../../components/abstract-date-picker/abstract-date-picker.component";
-import { NG_VALUE_ACCESSOR } from "@angular/forms";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+import { DateTime } from "luxon";
+import { Action } from "../../../../../utils/Action";
+import { PopupRef } from "../../../../../popup/models/PopupRef";
+import { faCalendar, IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { ConnectionPositionPair } from "@angular/cdk/overlay";
+import { take } from "rxjs";
 
 @Component({
     selector: "mona-date-picker",
@@ -25,16 +33,183 @@ import { NG_VALUE_ACCESSOR } from "@angular/forms";
         }
     ]
 })
-export class DatePickerComponent extends AbstractDatePickerComponent implements OnInit {
+export class DatePickerComponent implements OnInit, ControlValueAccessor {
+    #value: Date | null = null;
+    #propagateChange: Action<Date | null> | null = null;
+    public readonly dateIcon: IconDefinition = faCalendar;
+    private popupRef: PopupRef | null = null;
+    public currentDateString: string = "";
+    public navigatedDate: Date = new Date();
+
+    @HostBinding("class.mona-dropdown")
+    public readonly hostClass: boolean = true;
+
+    @ViewChild("datePopupTemplate")
+    public datePopupTemplateRef?: TemplateRef<any>;
+
     @Input()
-    public override format: string = "dd/MM/yyyy";
+    public disabled: boolean = false;
+
+    @Input()
+    public disabledDates: Iterable<Date> = [];
+
+    @Input()
+    public format: string = "dd/MM/yyyy";
+
+    @Input()
+    public max: Date | null = null;
+
+    @Input()
+    public min: Date | null = null;
+
+    @ViewChild("popupAnchor")
+    public popupAnchor!: ElementRef<HTMLDivElement>;
+
+    @Input()
+    public readonly: boolean = false;
 
     public constructor(
-        protected override readonly cdr: ChangeDetectorRef,
-        protected override readonly elementRef: ElementRef<HTMLElement>,
-        protected override readonly focusMonitor: FocusMonitor,
-        protected override readonly popupService: PopupService
-    ) {
-        super(cdr, elementRef, focusMonitor, popupService);
+        private readonly cdr: ChangeDetectorRef,
+        private readonly elementRef: ElementRef<HTMLElement>,
+        private readonly focusMonitor: FocusMonitor,
+        private readonly popupService: PopupService
+    ) {}
+
+    public ngOnInit(): void {
+        this.setDateValues();
+    }
+
+    public onCalendarValueChange(date: Date | null): void {
+        this.setCurrentDate(date);
+        this.popupRef?.close();
+    }
+
+    public onDateInputBlur(): void {
+        if (this.popupRef) {
+            return;
+        }
+        if (!this.currentDateString && this.value) {
+            this.setCurrentDate(null);
+            return;
+        }
+
+        const dateTime = DateTime.fromFormat(this.currentDateString, this.format);
+        if (this.dateStringEquals(this.value, dateTime.toJSDate())) {
+            return;
+        }
+        if (dateTime.isValid) {
+            if (this.value && DateTime.fromJSDate(this.value).equals(dateTime)) {
+                return;
+            }
+            if (this.min && dateTime.startOf("day") < DateTime.fromJSDate(this.min).startOf("day")) {
+                this.setCurrentDate(this.min);
+                return;
+            }
+            if (this.max && dateTime.startOf("day") > DateTime.fromJSDate(this.max).startOf("day")) {
+                this.setCurrentDate(this.max);
+                return;
+            }
+            this.setCurrentDate(dateTime.toJSDate());
+        } else {
+            if (this.value) {
+                this.currentDateString = DateTime.fromJSDate(this.value).toFormat(this.format);
+            } else {
+                this.currentDateString = "";
+            }
+        }
+        this.cdr.detectChanges();
+    }
+
+    public onDateInputButtonClick(): void {
+        if (!this.datePopupTemplateRef || this.readonly) {
+            return;
+        }
+
+        const input = this.elementRef.nativeElement.querySelector("input") as HTMLElement;
+        this.popupRef = this.popupService.create({
+            anchor: this.popupAnchor,
+            content: this.datePopupTemplateRef,
+            width: this.elementRef.nativeElement.getBoundingClientRect().width,
+            minWidth: 200,
+            popupClass: "mona-date-input-popup",
+            hasBackdrop: false,
+            withPush: false,
+            closeOnOutsideClick: true,
+            positions: [
+                new ConnectionPositionPair(
+                    { originX: "start", originY: "bottom" },
+                    { overlayX: "start", overlayY: "top" },
+                    -1,
+                    0,
+                    "mona-dropdown-popup-content-bottom"
+                ),
+                new ConnectionPositionPair(
+                    { originX: "start", originY: "top" },
+                    { overlayX: "start", overlayY: "bottom" },
+                    -1,
+                    -1,
+                    "mona-dropdown-popup-content-top"
+                )
+            ]
+        });
+        this.popupRef.closed.pipe(take(1)).subscribe(() => {
+            this.popupRef = null;
+            this.focusMonitor.focusVia(input, "program");
+        });
+    }
+
+    public onDateStringEdit(dateString: string): void {
+        this.currentDateString = dateString;
+    }
+
+    public registerOnChange(fn: any): void {
+        this.#propagateChange = fn;
+    }
+
+    public registerOnTouched(fn: any): void {}
+
+    public setDisabledState(isDisabled: boolean): void {
+        this.disabled = isDisabled;
+    }
+
+    public writeValue(date: Date | null | undefined): void {
+        this.#value = date ?? null;
+        if (date == null) {
+            this.currentDateString = "";
+        } else {
+            this.currentDateString = DateTime.fromJSDate(date).toFormat(this.format);
+        }
+        this.setDateValues();
+    }
+
+    private dateStringEquals(date1: Date | null, date2: Date | null): boolean {
+        if (date1 && date2) {
+            return (
+                DateTime.fromJSDate(date1).toFormat(this.format) === DateTime.fromJSDate(date2).toFormat(this.format)
+            );
+        }
+        return date1 === date2;
+    }
+
+    private setCurrentDate(date: Date | null): void {
+        this.#value = date;
+        if (date) {
+            this.currentDateString = DateTime.fromJSDate(date).toFormat(this.format);
+        } else {
+            this.currentDateString = "";
+        }
+        this.#propagateChange?.(date);
+        this.cdr.markForCheck();
+    }
+
+    private setDateValues(): void {
+        this.navigatedDate = this.value ?? DateTime.now().toJSDate();
+        if (this.value) {
+            this.currentDateString = DateTime.fromJSDate(this.value).toFormat(this.format);
+        }
+    }
+
+    public get value(): Date | null {
+        return this.#value;
     }
 }
