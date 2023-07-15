938601a344c07c873687e76d2223369da4480ab6
Nanahoshi <nanahoshii@outlook.com>
2023-07-15 16:12:47 +0300

    refactor: simplify pipe code

--- projects/mona-ui/src/lib/date-inputs/modules/time-selector/pipes/time-limiter.pipe.ts
+++ projects/mona-ui/src/lib/date-inputs/modules/time-selector/pipes/time-limiter.pipe.ts
@@ -60,64 +60,26 @@ export class TimeLimiterPipe implements PipeTransform {
         return result;
     }
 
-    private cloneDate(date: Date): Date {
-        return new Date(date.getTime());
-    }
-
     private normalizeDates(
         date: Date,
         min?: Date | null,
         max?: Date | null
     ): { minDate?: Date; maxDate?: Date; date: Date } {
-        if (!min && !max) {
-            return { minDate: date, maxDate: date, date };
-        }
-        if (min && !max) {
+        let minDate, maxDate, newDate;
+        if (min) {
             const minYears = Math.min(min.getFullYear(), date.getFullYear());
             const minMonths = Math.min(min.getMonth(), date.getMonth());
             const minDays = Math.min(min.getDate(), date.getDate());
-            const newMin = new Date(minYears, minMonths, minDays, min.getHours(), min.getMinutes(), min.getSeconds());
-            const newDate = new Date(
-                minYears,
-                minMonths,
-                minDays,
-                date.getHours(),
-                date.getMinutes(),
-                date.getSeconds()
-            );
-            return { minDate: newMin, maxDate: undefined, date: newDate };
+            minDate = new Date(minYears, minMonths, minDays, min.getHours(), min.getMinutes(), min.getSeconds());
+            newDate = new Date(minYears, minMonths, minDays, date.getHours(), date.getMinutes(), date.getSeconds());
         }
-        if (!min && max) {
+        if (max) {
             const maxYears = Math.max(max.getFullYear(), date.getFullYear());
             const maxMonths = Math.max(max.getMonth(), date.getMonth());
             const maxDays = Math.max(max.getDate(), date.getDate());
-            const newMax = new Date(maxYears, maxMonths, maxDays, max.getHours(), max.getMinutes(), max.getSeconds());
-            const newDate = new Date(
-                maxYears,
-                maxMonths,
-                maxDays,
-                date.getHours(),
-                date.getMinutes(),
-                date.getSeconds()
-            );
-            return { minDate: undefined, maxDate: newMax, date: newDate };
+            maxDate = new Date(maxYears, maxMonths, maxDays, max.getHours(), max.getMinutes(), max.getSeconds());
+            newDate = new Date(maxYears, maxMonths, maxDays, date.getHours(), date.getMinutes(), date.getSeconds());
         }
-        if (min && max) {
-            const minYears = Math.min(min.getFullYear(), date.getFullYear());
-            const minMonths = Math.min(min.getMonth(), date.getMonth());
-            const minDays = Math.min(min.getDate(), date.getDate());
-            const newMin = new Date(minYears, minMonths, minDays, min.getHours(), min.getMinutes(), min.getSeconds());
-            const newMax = new Date(minYears, minMonths, minDays, max.getHours(), max.getMinutes(), max.getSeconds());
-            const newDate = new Date(
-                minYears,
-                minMonths,
-                minDays,
-                date.getHours(),
-                date.getMinutes(),
-                date.getSeconds()
-            );
-            return { minDate: newMin, maxDate: newMax, date: newDate };
-        }
-        return { minDate: date, maxDate: date, date };
+        return { minDate, maxDate, date: newDate || date };
     }
 }
