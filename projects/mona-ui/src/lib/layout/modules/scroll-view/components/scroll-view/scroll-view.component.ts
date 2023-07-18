c2cc3581838803a35a78ab4efefd53c44f81ba61
Nanahoshi <nanahoshii@outlook.com>
2023-07-19 00:13:18 +0300

    Scroll view pager

--- projects/mona-ui/src/lib/layout/modules/scroll-view/components/scroll-view/scroll-view.component.ts
+++ projects/mona-ui/src/lib/layout/modules/scroll-view/components/scroll-view/scroll-view.component.ts
@@ -3,16 +3,19 @@ import {
     Component,
     computed,
     ContentChild,
+    ElementRef,
     HostBinding,
     Input,
     OnInit,
-    Signal,
     signal,
     TemplateRef,
     WritableSignal
 } from "@angular/core";
 import { faChevronLeft, faChevronRight, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { ScrollViewListItem } from "../../models/ScrollViewListItem";
+import { PagerOverlay } from "../../models/PagerOverlay";
+import { interval, Subject, takeUntil, timer } from "rxjs";
+import { ScrollDirection } from "../../../../../models/ScrollDirection";
 
 @Component({
     selector: "mona-scroll-view",
@@ -23,10 +26,12 @@ import { ScrollViewListItem } from "../../models/ScrollViewListItem";
 export class ScrollViewComponent implements OnInit {
     #data: WritableSignal<Array<ScrollViewListItem>> = signal([]);
     #height: WritableSignal<string> = signal("100%");
+    #scroll$: Subject<void> = new Subject<void>();
     #width: WritableSignal<string> = signal("100%");
     public readonly leftArrow: IconDefinition = faChevronLeft;
     public readonly rightArrow: IconDefinition = faChevronRight;
     public activeIndex: WritableSignal<number> = signal(0);
+    public pagerPosition: WritableSignal<string> = signal("0");
 
     @ContentChild(TemplateRef)
     public contentTemplate: TemplateRef<any> | null = null;
@@ -50,6 +55,12 @@ export class ScrollViewComponent implements OnInit {
         return this.#height();
     }
 
+    @Input()
+    public pagerBlur: number = 3;
+
+    @Input()
+    public pagerOverlay: PagerOverlay = "dark";
+
     @Input({ required: true })
     @HostBinding("style.width")
     public set width(value: string | number) {
@@ -60,7 +71,7 @@ export class ScrollViewComponent implements OnInit {
         return this.#width();
     }
 
-    public constructor() {}
+    public constructor(private readonly elementRef: ElementRef<HTMLElement>) {}
 
     public ngOnInit(): void {}
 
@@ -73,6 +84,40 @@ export class ScrollViewComponent implements OnInit {
             index = Math.min(this.#data().length - 1, index + 1);
             this.activeIndex.set(index);
         }
+        const element = this.elementRef.nativeElement.querySelector("li.mona-scroll-view-active-page");
+        if (element) {
+            element.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "center" });
+        }
+    }
+
+    public onPageClick(index: number, element: HTMLLIElement): void {
+        this.activeIndex.set(index);
+        element.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "center" });
+    }
+
+    public onPagerScroll(element: HTMLUListElement, direction: ScrollDirection, type: "single" | "continuous"): void {
+        const timeFunction = type === "single" ? timer : interval;
+        timeFunction(60)
+            .pipe(takeUntil(this.#scroll$))
+            .subscribe(() => {
+                let left: number = 0;
+                switch (direction) {
+                    case "left":
+                        left = Math.max(element.scrollLeft - 100, 0);
+                        element.scrollTo({ left, behavior: "smooth" });
+                        break;
+                    case "right":
+                        left = Math.min(element.scrollLeft + 100, element.scrollWidth);
+                        element.scrollTo({ left, behavior: "smooth" });
+                        break;
+                }
+            });
+    }
+
+    public onPagerScrollEnd(): void {
+        this.#scroll$.next();
+        this.#scroll$.complete();
+        this.#scroll$ = new Subject<void>();
     }
 
     private setData(data: Iterable<any>): void {
---
+++ projects/mona-ui/src/lib/layout/modules/scroll-view/models/PagerOverlay.ts
@@ -0,0 +1,1 @@
+export type PagerOverlay = "dark" | "light";
