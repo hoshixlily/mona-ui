5dbedab20325bf916ea1ab57aab549b339e1ff2f
Nanahoshi <nanahoshii@outlook.com>
2023-07-24 05:13:21 +0300

    Color picker animations

--- projects/mona-ui/src/lib/inputs/modules/color-picker/components/color-picker/color-picker.component.ts
+++ projects/mona-ui/src/lib/inputs/modules/color-picker/components/color-picker/color-picker.component.ts
@@ -1,7 +1,21 @@
-import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from "@angular/core";
+import {
+    Component,
+    ElementRef,
+    EventEmitter,
+    forwardRef,
+    Input,
+    OnInit,
+    Output,
+    TemplateRef,
+    ViewChild
+} from "@angular/core";
 import { faChevronDown, faTimes, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 import { Action } from "../../../../../utils/Action";
+import { PopupRef } from "../../../../../popup/models/PopupRef";
+import { PopupAnimationService } from "../../../../../animations/popup-animation.service";
+import { PopupService } from "../../../../../popup/services/popup.service";
+import { AnimationState } from "../../../../../animations/AnimationState";
 
 @Component({
     selector: "mona-color-picker",
@@ -16,24 +30,47 @@ import { Action } from "../../../../../utils/Action";
     ]
 })
 export class ColorPickerComponent implements OnInit, ControlValueAccessor {
+    private popupRef: PopupRef | null = null;
     private propagateChange: Action<string | null> | null = null;
     public readonly noColorIcon: IconDefinition = faTimes;
     public readonly dropdownIcon: IconDefinition = faChevronDown;
     public color: string | null = null;
 
+    @ViewChild("colorPickerAnchor")
+    public colorPickerAnchor!: ElementRef<HTMLDivElement>;
+
     @Input()
     public palette: string[] = [];
+
+    @ViewChild("popupTemplate")
+    public popupTemplateRef!: TemplateRef<any>;
 
     @Output()
     public valueChange: EventEmitter<string | null> = new EventEmitter<string | null>();
 
-    public constructor() {}
+    public constructor(
+        private readonly popupAnimationService: PopupAnimationService,
+        private readonly popupService: PopupService
+    ) {}
 
     public ngOnInit(): void {}
 
     public onColorPaletteValueChange(value: string | null): void {
         this.color = value;
         this.propagateChange?.(value);
+    }
+
+    public open(): void {
+        this.popupRef = this.popupService.create({
+            anchor: this.colorPickerAnchor,
+            content: this.popupTemplateRef,
+            popupClass: "mona-color-picker-popup",
+            width: "auto",
+            hasBackdrop: false,
+            closeOnOutsideClick: false
+        });
+        this.popupAnimationService.setupDropdownOutsideClickCloseAnimation(this.popupRef);
+        this.popupAnimationService.animateDropdown(this.popupRef, AnimationState.Show);
     }
 
     public registerOnChange(fn: any): void {
