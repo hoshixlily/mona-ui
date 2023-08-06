58b752112648ad7e8cebe9c22d77417d4283d30a
Nanahoshi <nanahoshii@outlook.com>
2023-08-06 03:46:07 +0300

    Add three predefined palettes

--- projects/mona-ui/src/lib/inputs/modules/color-picker/color-picker.module.ts
+++ projects/mona-ui/src/lib/inputs/modules/color-picker/color-picker.module.ts
@@ -5,10 +5,11 @@ import { PopupModule } from "../../../popup/popup.module";
 import { ColorPaletteModule } from "../color-palette/color-palette.module";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import {ButtonModule} from "../../../buttons/modules/button/button.module";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     declarations: [ColorPickerComponent],
-    imports: [CommonModule, PopupModule, ColorPaletteModule, FontAwesomeModule, ButtonModule],
+    imports: [CommonModule, PopupModule, ColorPaletteModule, FontAwesomeModule, ButtonModule, FormsModule],
     exports: [ColorPickerComponent]
 })
 export class ColorPickerModule {}
