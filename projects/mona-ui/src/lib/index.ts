167c78ab084fc1012c0601e5bc75716cca5695b0
Nanahoshi <nanahoshii@outlook.com>
2023-08-07 01:37:09 +0300

    build: ng serve will now watch library changes as well
    No need for build:watch for watching library changes

---
+++ projects/mona-ui/src/lib/index.ts
@@ -0,0 +1,266 @@
+/*
+ * Public API Surface of mona-ui
+ */
+
+/** Buttons */
+export * from "./buttons/modules/button/directives/button.directive";
+export * from "./buttons/modules/button/button.module";
+export * from "./buttons/modules/button-group/components/button-group/button-group.component";
+export * from "./buttons/modules/button-group/button-group.module";
+export * from "./buttons/modules/chip/components/chip/chip.component";
+export * from "./buttons/modules/chip/chip.module";
+export * from "./buttons/modules/split-button/directives/split-button-text-template.directive";
+export * from "./buttons/modules/split-button/components/split-button/split-button.component";
+export * from "./buttons/modules/split-button/split-button.module";
+export * from "./buttons/buttons.module";
+
+/** Date Inputs */
+export * from "./date-inputs/modules/calendar/components/calendar/calendar.component";
+export * from "./date-inputs/modules/calendar/calendar.module";
+export * from "./date-inputs/modules/date-picker/components/date-picker/date-picker.component";
+export * from "./date-inputs/modules/date-picker/date-picker.module";
+export * from "./date-inputs/modules/date-time-picker/components/date-time-picker/date-time-picker.component";
+export * from "./date-inputs/modules/date-time-picker/date-time-picker.module";
+export * from "./date-inputs/modules/time-picker/components/time-picker/time-picker.component";
+export * from "./date-inputs/modules/time-picker/time-picker.module";
+export * from "./date-inputs/date-inputs.module";
+
+/** Dropdowns */
+
+export * from "./dropdowns/components/popup-list/popup-list.component";
+export * from "./dropdowns/directives/list-group-template.directive";
+export * from "./dropdowns/directives/list-item-template.directive";
+
+export * from "./dropdowns/modules/auto-complete/components/auto-complete/auto-complete.component";
+export * from "./dropdowns/modules/auto-complete/auto-complete.module";
+
+export * from "./dropdowns/modules/combo-box/directives/combo-box-group-template.directive";
+export * from "./dropdowns/modules/combo-box/directives/combo-box-item-template.directive";
+export * from "./dropdowns/modules/combo-box/components/combo-box/combo-box.component";
+export * from "./dropdowns/modules/combo-box/combo-box.module";
+//
+export * from "./dropdowns/modules/drop-down-list/directives/drop-down-list-group-template.directive";
+export * from "./dropdowns/modules/drop-down-list/directives/drop-down-list-item-template.directive";
+export * from "./dropdowns/modules/drop-down-list/directives/drop-down-list-value-template.directive";
+export * from "./dropdowns/modules/drop-down-list/components/drop-down-list/drop-down-list.component";
+export * from "./dropdowns/modules/drop-down-list/drop-down-list.module";
+//
+export * from "./dropdowns/modules/multi-select/directives/multi-select-group-template.directive";
+export * from "./dropdowns/modules/multi-select/directives/multi-select-item-template.directive";
+export * from "./dropdowns/modules/multi-select/directives/multi-select-summary-tag.directive";
+export * from "./dropdowns/modules/multi-select/directives/multi-select-summary-tag-template.directive";
+export * from "./dropdowns/modules/multi-select/directives/multi-select-tag-template.directive";
+export * from "./dropdowns/modules/multi-select/components/multi-select/multi-select.component";
+export * from "./dropdowns/modules/multi-select/multi-select.module";
+
+export * from "./dropdowns/drop-downs.module";
+
+/** Filters */
+export * from "./query/filter/FilterDescriptor";
+export { Query, IQuery } from "./query/core/Query";
+export * from "./models/DataType";
+export * from "./filter/models/FilterMenuValue";
+export * from "./filter/components/filter-menu/filter-menu.component";
+export * from "./filter/filter.module";
+
+/** Grid */
+export * from "./query/sort/SortDescriptor";
+export * from "./grid/models/CellEditEvent";
+export * from "./grid/models/SelectableOptions";
+export * from "./grid/models/SortableOptions";
+export * from "./grid/directives/grid-editable.directive";
+export * from "./grid/directives/grid-selectable.directive";
+export * from "./grid/directives/grid-cell-template.directive";
+export * from "./grid/directives/grid-column-title-template.directive";
+export * from "./grid/components/grid-column/grid-column.component";
+export * from "./grid/components/grid/grid.component";
+export * from "./grid/grid.module";
+
+/** Inputs */
+export * from "./inputs/modules/check-box/directives/check-box.directive";
+export * from "./inputs/modules/check-box/check-box.module";
+
+export * from "./inputs/modules/color-gradient/components/color-gradient/color-gradient.component";
+export * from "./inputs/modules/color-gradient/color-gradient.module";
+
+export * from "./inputs/modules/color-palette/components/color-palette/color-palette.component";
+export * from "./inputs/modules/color-palette/color-palette.module";
+
+export * from "./inputs/modules/color-picker/components/color-picker/color-picker.component";
+export * from "./inputs/modules/color-picker/color-picker.module";
+
+export * from "./inputs/modules/numeric-text-box/directives/numeric-text-box-prefix-template.directive";
+export * from "./inputs/modules/numeric-text-box/components/numeric-text-box/numeric-text-box.component";
+export * from "./inputs/modules/numeric-text-box/numeric-text-box.module";
+
+export * from "./inputs/modules/radio-button/directives/radio-button.directive";
+export * from "./inputs/modules/radio-button/radio-button.module";
+
+export * from "./inputs/modules/range-slider/directives/range-slider-tick-value-template.directive";
+export * from "./inputs/modules/range-slider/components/range-slider/range-slider.component";
+export * from "./inputs/modules/range-slider/range-slider.module";
+export * from "./inputs/modules/slider/directives/slider-tick-value-template.directive";
+export * from "./inputs/modules/slider/components/slider/slider.component";
+export * from "./inputs/modules/slider/slider.module";
+
+export * from "./inputs/modules/switch/directives/switch-off-label-template.directive";
+export * from "./inputs/modules/switch/directives/switch-on-label-template.directive";
+export * from "./inputs/modules/switch/components/switch/switch.component";
+export * from "./inputs/modules/switch/switch.module";
+
+export * from "./inputs/modules/text-area/directives/text-area.directive";
+export * from "./inputs/modules/text-area/text-area.module";
+
+export * from "./inputs/modules/text-box/directives/text-box.directive";
+export * from "./inputs/modules/text-box/directives/text-box-prefix-template.directive";
+export * from "./inputs/modules/text-box/directives/text-box-suffix-template.directive";
+export * from "./inputs/modules/text-box/components/text-box/text-box.component";
+export * from "./inputs/modules/text-box/text-box.module";
+export * from "./inputs/inputs.module";
+
+/** Layout */
+
+/** Avatar */
+export * from "./layout/modules/avatar/components/avatar/avatar.component";
+export * from "./layout/modules/avatar/avatar.module";
+
+/* Expansion Panel */
+export * from "./layout/modules/expansion-panel/components/expansion-panel/expansion-panel.component";
+export * from "./layout/modules/expansion-panel/directives/expansion-panel-actions-template.directive";
+export * from "./layout/modules/expansion-panel/directives/expansion-panel-title-template.directive";
+export * from "./layout/modules/expansion-panel/expansion-panel.module";
+
+/* Fieldset */
+export * from "./layout/modules/fieldset/directives/fieldset-legend-template.directive";
+export * from "./layout/modules/fieldset/components/fieldset/fieldset.component";
+export * from "./layout/modules/fieldset/fieldset.module";
+
+/** Scroll View */
+export * from "./layout/modules/scroll-view/components/scroll-view/scroll-view.component";
+export * from "./layout/modules/scroll-view/scroll-view.module";
+
+/** Stepper */
+export { StepOptions } from "./layout/modules/stepper/models/Step";
+export * from "./layout/modules/stepper/directives/stepper-indicator-template.directive";
+export * from "./layout/modules/stepper/directives/stepper-label-template.directive";
+export * from "./layout/modules/stepper/directives/stepper-step-template.directive";
+export * from "./layout/modules/stepper/components/stepper/stepper.component";
+export * from "./layout/modules/stepper/stepper.module";
+
+/** Splitter */
+export * from "./layout/modules/splitter/components/splitter/splitter.component";
+export * from "./layout/modules/splitter/components/splitter-pane/splitter-pane.component";
+export * from "./layout/modules/splitter/splitter.module";
+
+/* Tab Strip */
+export * from "./layout/modules/tab-strip/data/TabCloseEvent";
+export * from "./layout/modules/tab-strip/directives/tab-content-template.directive";
+export * from "./layout/modules/tab-strip/directives/tab-title-template.directive";
+export * from "./layout/modules/tab-strip/components/tab-strip/tab-strip.component";
+export * from "./layout/modules/tab-strip/components/tab/tab.component";
+export * from "./layout/modules/tab-strip/tab-strip.module";
+export * from "./layout/layout.module";
+/** Layout End */
+
+/** Menus */
+// export * from "./menus/models/MenuItem";
+export * from "./menus/modules/shared-menu/components/menu-item/menu-item.component";
+export * from "./menus/modules/shared-menu/directives/menu-item-icon-template.directive";
+// export * from "./menus/modules/shared-menu/directives/menu-item-link-template.directive";
+export * from "./menus/modules/shared-menu/directives/menu-item-text-template.directive";
+export * from "./menus/modules/context-menu/components/context-menu/context-menu.component";
+export * from "./menus/modules/context-menu/context-menu.module";
+export * from "./menus/modules/menubar/directives/menu-text-template.directive";
+export * from "./menus/modules/menubar/components/menu/menu.component";
+export * from "./menus/modules/menubar/components/menubar/menubar.component";
+export * from "./menus/modules/menubar/menubar.module";
+export * from "./menus/menus.module";
+
+/** Navigation */
+
+/** Breadcrumb */
+export * from "./navigation/modules/breadcrumb/models/BreadcrumbItem";
+export * from "./navigation/modules/breadcrumb/directives/breadcrumb-item-template.directive";
+export * from "./navigation/modules/breadcrumb/directives/breadcrumb-separator-template.directive";
+export * from "./navigation/modules/breadcrumb/components/breadcrumb/breadcrumb.component";
+export * from "./navigation/modules/breadcrumb/breadcrumb.module";
+export * from "./navigation/navigation.module";
+
+/** Notification */
+export * from "./notification/services/notification.service";
+export * from "./notification/components/notification/notification.component";
+export * from "./notification/notification.module";
+
+/** Pager */
+export * from "./pager/models/PageChangeEvent";
+export * from "./pager/models/PageSizeChangeEvent";
+export * from "./pager/components/pager/pager.component";
+export * from "./pager/pager.module";
+
+/** Popup */
+export { PopupRef } from "./popup/models/PopupRef";
+export * from "./popup/models/PopupSettings";
+export * from "./popup/services/popup.service";
+export * from "./popup/components/popup/popup.component";
+export * from "./popup/popup.module";
+
+/** Progress */
+export * from "./progress-bars/modules/circular-progress-bar/directives/circular-progress-bar-label-template.directive";
+export * from "./progress-bars/modules/circular-progress-bar/components/circular-progress-bar/circular-progress-bar.component";
+export * from "./progress-bars/modules/circular-progress-bar/circular-progress-bar.module";
+export * from "./progress-bars/modules/progress-bar/components/progress-bar/progress-bar.component";
+export * from "./progress-bars/modules/progress-bar/progress-bar.module";
+export * from "./progress-bars/progress-bars.module";
+
+/** Tooltips */
+
+/** Popover */
+export * from "./tooltips/modules/popover/models/PopoverHideEvent";
+export * from "./tooltips/modules/popover/models/PopoverShowEvent";
+export * from "./tooltips/modules/popover/models/PopoverShownEvent";
+export * from "./tooltips/modules/popover/directives/popover-footer-template.directive";
+export * from "./tooltips/modules/popover/directives/popover-title-template.directive";
+export * from "./tooltips/modules/popover/components/popover/popover.component";
+export * from "./tooltips/modules/popover/popover.module";
+
+/** Tooltip */
+export * from "./tooltips/modules/tooltip/components/tooltip/tooltip.component";
+export * from "./tooltips/modules/tooltip/tooltip.module";
+export * from "./tooltips/tooltips.module";
+
+/** TreeView */
+export { NodeOptions } from "./tree-view/data/Node";
+export * from "./tree-view/data/NodeClickEvent";
+export * from "./tree-view/data/NodeDragEndEvent";
+export * from "./tree-view/data/NodeDragStartEvent";
+export * from "./tree-view/data/NodeDragEvent";
+export * from "./tree-view/data/NodeDropEvent";
+export * from "./tree-view/data/NodeLookupItem";
+export * from "./tree-view/data/CheckableOptions";
+export * from "./tree-view/components/tree-view/tree-view.component";
+export * from "./tree-view/directives/tree-view-checkable.directive";
+export * from "./tree-view/directives/tree-view-disable.directive";
+export * from "./tree-view/directives/tree-view-expandable.directive";
+export * from "./tree-view/directives/tree-view-selectable.directive";
+export * from "./tree-view/directives/tree-view-node-text-template.directive";
+export * from "./tree-view/tree-view.module";
+
+/** Window */
+export * from "./window/models/DialogAction";
+export * from "./window/models/DialogRef";
+export * from "./window/models/DialogResult";
+export * from "./window/models/DialogSettings";
+export * from "./window/models/DialogType";
+export * from "./window/services/dialog.service";
+export { WindowRef } from "./window/models/WindowRef";
+export * from "./window/models/WindowSettings";
+export * from "./window/services/window.service";
+export * from "./window/directives/window-title-template.directive";
+export * from "./window/components/window/window.component";
+export * from "./window/window.module";
+
+/** Shared [TEMPORARY EXPORT] */
+// export * from "./shared/components/popup-list-item/popup-list-item.component";
+export * from "./shared/shared.module";
+
+export * from "./mona-ui.module";
