84048532ac37ddc74328cbda1efe87bb7ec011cb
Nanahoshi <nanahoshii@outlook.com>
2023-08-26 19:17:47 +0300

    fix: fix imports

--- projects/mona-ui-tester/src/main.ts
+++ projects/mona-ui-tester/src/main.ts
@@ -1,25 +1,21 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgOptimizedImage } from "@angular/common";
+import { enableProdMode, importProvidersFrom } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { AppRoutingModule } from "./app/app-routing.module";
+import { AppComponent } from "./app/app.component";
 
-
-import { environment } from './environments/environment';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { NgOptimizedImage } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ButtonsModule, DropDownsModule, InputsModule, PopupModule, ContextMenuModule, LayoutModule, TreeViewModule, DateInputsModule, TooltipsModule, MenubarModule, WindowModule, ProgressBarsModule, PagerModule, FilterModule, GridModule, NotificationModule, NavigationModule, ListViewModule, ListBoxModule } from 'mona-ui';
-import { AppRoutingModule } from './app/app-routing.module';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { environment } from "./environments/environment";
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(AppRoutingModule, ButtonsModule, DropDownsModule, InputsModule, PopupModule, ContextMenuModule, FontAwesomeModule, FormsModule, LayoutModule, TreeViewModule, DateInputsModule, TooltipsModule, MenubarModule, WindowModule, ProgressBarsModule, PagerModule, FilterModule, GridModule, NotificationModule, NgOptimizedImage, NavigationModule, ListViewModule, ListBoxModule),
+        importProvidersFrom(AppRoutingModule, FontAwesomeModule, FormsModule, NgOptimizedImage),
         provideAnimations()
     ]
-})
-  .catch(err => console.error(err));
+}).catch(err => console.error(err));
