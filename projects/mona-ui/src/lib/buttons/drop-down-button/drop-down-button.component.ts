16f75bc9387f299882c90eb62bcab16e239f3a71
Nanahoshi <nanahoshii@outlook.com>
2023-09-17 03:44:17 +0300

    fix: drop down button console error

--- projects/mona-ui/src/lib/buttons/drop-down-button/drop-down-button.component.ts
+++ projects/mona-ui/src/lib/buttons/drop-down-button/drop-down-button.component.ts
@@ -4,10 +4,13 @@ import {
     Component,
     ContentChildren,
     DestroyRef,
+    ElementRef,
     inject,
     Input,
     QueryList,
-    ViewChild
+    signal,
+    ViewChild,
+    WritableSignal
 } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { ContextMenuComponent } from "../../menus/context-menu/context-menu.component";
@@ -25,7 +28,9 @@ import { ButtonDirective } from "../button/button.directive";
 })
 export class DropDownButtonComponent implements AfterViewInit, AfterContentInit {
     readonly #destroyRef: DestroyRef = inject(DestroyRef);
+    #resizeObserver: ResizeObserver | null = null;
     public menuItems: MenuItem[] = [];
+    public popupWidth: WritableSignal<number> = signal(0);
 
     @ViewChild("contextMenuComponent")
     private readonly contextMenuComponent!: ContextMenuComponent;
@@ -33,8 +38,17 @@ export class DropDownButtonComponent implements AfterViewInit, AfterContentInit 
     @Input()
     public disabled: boolean = false;
 
+    @ViewChild("dropdownButton", { read: ElementRef })
+    public buttonElement!: ElementRef<HTMLButtonElement>;
+
     @ContentChildren(MenuItemComponent)
     public menuItemComponents: QueryList<MenuItemComponent> = new QueryList<MenuItemComponent>();
+
+    public constructor() {
+        this.#destroyRef.onDestroy(() => {
+            this.#resizeObserver?.disconnect();
+        });
+    }
 
     public ngAfterContentInit(): void {
         this.menuItems = this.menuItemComponents.map(m => m.getMenuItem());
@@ -46,6 +60,11 @@ export class DropDownButtonComponent implements AfterViewInit, AfterContentInit 
     public ngAfterViewInit(): void {
         window.setTimeout(() => {
             this.contextMenuComponent.setPrecise(false);
+            this.popupWidth.set(this.buttonElement.nativeElement.offsetWidth);
         });
+        this.#resizeObserver = new ResizeObserver(() => {
+            this.popupWidth.set(this.buttonElement.nativeElement.offsetWidth);
+        });
+        this.#resizeObserver.observe(this.buttonElement.nativeElement);
     }
 }
