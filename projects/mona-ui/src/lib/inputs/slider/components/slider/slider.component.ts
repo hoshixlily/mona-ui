207f4e757fbacb496881e68294fa34fd3502db57
Nanahoshi <nanahoshilily@outlook.com>
2024-02-18 23:56:24 +0300

    refactor: slider style updates

--- projects/mona-ui/src/lib/inputs/slider/components/slider/slider.component.ts
+++ projects/mona-ui/src/lib/inputs/slider/components/slider/slider.component.ts
@@ -1,30 +1,30 @@
+import { NgClass, NgTemplateOutlet } from "@angular/common";
 import {
     AfterViewInit,
     ChangeDetectionStrategy,
     Component,
+    computed,
     ContentChild,
     DestroyRef,
     ElementRef,
     forwardRef,
     inject,
-    Input,
+    input,
+    InputSignal,
     NgZone,
-    OnChanges,
-    OnInit,
+    Signal,
     signal,
-    SimpleChanges,
     TemplateRef,
     ViewChild,
     WritableSignal
 } from "@angular/core";
-import { SliderTick } from "../../../models/SliderTick";
-import { SliderLabelPosition } from "../../../models/SliderLabelPosition";
-import { SliderTickValueTemplateDirective } from "../../directives/slider-tick-value-template.directive";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 import { delay, filter, fromEvent, take, tap } from "rxjs";
 import { Action } from "../../../../utils/Action";
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
-import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { NgClass, NgIf, NgFor, NgTemplateOutlet } from "@angular/common";
+import { SliderLabelPosition } from "../../../models/SliderLabelPosition";
+import { SliderTick } from "../../../models/SliderTick";
+import { SliderTickValueTemplateDirective } from "../../directives/slider-tick-value-template.directive";
 
 @Component({
     selector: "mona-slider",
@@ -39,68 +39,61 @@ import { NgClass, NgIf, NgFor, NgTemplateOutlet } from "@angular/common";
         }
     ],
     standalone: true,
-    imports: [NgClass, NgIf, NgFor, NgTemplateOutlet]
+    imports: [NgClass, NgTemplateOutlet],
+    host: {
+        "[class.mona-slider]": "true",
+        "[class.mona-slider-horizontal]": "orientation() === 'horizontal'",
+        "[class.mona-slider-vertical]": "orientation() === 'vertical'",
+        "[class.mona-disabled]": "disabled()"
+    }
 })
-export class SliderComponent implements OnInit, AfterViewInit, ControlValueAccessor, OnChanges {
+export class SliderComponent implements AfterViewInit, ControlValueAccessor {
     readonly #destroyRef: DestroyRef = inject(DestroyRef);
+    readonly #hostElementRef: ElementRef<HTMLDivElement> = inject(ElementRef);
+    readonly #zone: NgZone = inject(NgZone);
     #mouseDown: boolean = false;
     #mouseMove: boolean = false;
     #propagateChange: Action<number> | null = null;
-    public dragging: WritableSignal<boolean> = signal(false);
-    public handlePosition: WritableSignal<number> = signal(0);
-    public handleValue: WritableSignal<number> = signal(0);
-    public ticks: SliderTick[] = [];
+    protected readonly dragging: WritableSignal<boolean> = signal(false);
+    protected readonly handlePosition: WritableSignal<number> = signal(0);
+    protected readonly handleValue: WritableSignal<number> = signal(0);
+    protected readonly ticks: Signal<SliderTick[]> = computed(() => {
+        const min = this.min();
+        const max = this.max();
+        const tickList: SliderTick[] = [];
+        let value = min;
+        let index = 0;
+        while (value < max) {
+            tickList.push({ index, value });
+            value += this.step();
+            index++;
+        }
+        tickList.push({ index, value: Math.min(value + this.step(), max) });
+        if (this.orientation() === "vertical") {
+            tickList.reverse();
+        }
+        return tickList;
+    });
 
-    @Input()
-    public disabled: boolean = false;
-
-    @Input()
-    public labelPosition: SliderLabelPosition = "after";
-
-    @Input()
-    public labelStep: number = 1;
-
-    @Input()
-    public max: number = 10;
-
-    @Input()
-    public min: number = 0;
-
-    @Input()
-    public orientation: "horizontal" | "vertical" = "horizontal";
-
-    @Input()
-    public showLabels: boolean = false;
-
-    @Input()
-    public showTicks: boolean = false;
+    public disabled: InputSignal<boolean> = input(false);
+    public labelPosition: InputSignal<SliderLabelPosition> = input<SliderLabelPosition>("after");
+    public labelStep: InputSignal<number> = input(1);
+    public max: InputSignal<number> = input(10);
+    public min: InputSignal<number> = input(0);
+    public orientation: InputSignal<"horizontal" | "vertical"> = input<"horizontal" | "vertical">("horizontal");
+    public showLabels: InputSignal<boolean> = input(false);
+    public showTicks: InputSignal<boolean> = input(false);
+    public step: InputSignal<number> = input(1);
+    public tickStep: InputSignal<number> = input(1);
 
     @ViewChild("sliderHandle")
     public sliderHandle!: ElementRef<HTMLDivElement>;
 
-    @Input()
-    public step: number = 1;
-
-    @Input()
-    public tickStep: number = 1;
-
     @ContentChild(SliderTickValueTemplateDirective, { read: TemplateRef })
     public tickValueTemplate?: TemplateRef<any>;
 
-    public constructor(private readonly elementRef: ElementRef<HTMLDivElement>, private readonly zone: NgZone) {}
-
     public ngAfterViewInit(): void {
         this.setSubscription();
-    }
-
-    public ngOnChanges(changes: SimpleChanges): void {
-        if (changes["min"] || changes["max"] || changes["step"]) {
-            this.prepareTicks(); // TODO: Use setters and signals to update ticks
-        }
-    }
-
-    public ngOnInit(): void {
-        this.prepareTicks();
     }
 
     public registerOnChange(fn: any): void {
@@ -111,7 +104,7 @@ export class SliderComponent implements OnInit, AfterViewInit, ControlValueAcces
 
     public writeValue(obj: number): void {
         if (obj != null) {
-            const value = Math.max(this.min, Math.min(obj, this.max));
+            const value = Math.max(this.min(), Math.min(obj, this.max()));
             const position = this.getPositionFromValue(value);
             this.handleValue.set(value);
             this.handlePosition.set(position);
@@ -119,45 +112,28 @@ export class SliderComponent implements OnInit, AfterViewInit, ControlValueAcces
     }
 
     private getPositionFromValue(value: number): number {
-        const containerRect = this.elementRef.nativeElement.getBoundingClientRect();
-        if (this.orientation === "horizontal") {
-            if (value === this.min) {
-                return 0;
-            }
-            if (value === this.max) {
-                return containerRect.width;
-            }
-            return ((value - this.min) / (this.max - this.min)) * containerRect.width;
-        } else {
-            if (value === this.min) {
-                return 0;
-            }
-            if (value === this.max) {
-                return containerRect.height;
-            }
-            return ((value - this.min) / (this.max - this.min)) * containerRect.height;
+        if (value === this.min()) {
+            return 0;
         }
+        if (value === this.max()) {
+            return 100;
+        }
+        return ((value - this.min()) / (this.max() - this.min())) * 100;
     }
 
     private getValueFromPosition(position: number): number {
-        const containerRect = this.elementRef.nativeElement.getBoundingClientRect();
-        if (this.orientation === "horizontal") {
-            const value = position / containerRect.width;
-            return Math.max(this.min, Math.min(this.max, Math.round(value * this.max)));
-        } else {
-            const value = position / containerRect.height;
-            return Math.max(this.min, Math.min(this.max, Math.round(value * this.max)));
-        }
+        const value = position / 100;
+        return Math.max(this.min(), Math.min(this.max(), Math.round(value * this.max())));
     }
 
     private handleHandleMove(event: MouseEvent, direction: "horizontal" | "vertical"): void {
-        if (this.showTicks) {
+        if (this.showTicks()) {
             const tick = this.findClosestTickElement(event);
             const valueStr = tick.getAttribute("data-value");
             const value = valueStr ? Number(valueStr) : 0;
             const position = this.getPositionFromValue(value);
             if (position !== this.handlePosition()) {
-                this.zone.run(() => {
+                this.#zone.run(() => {
                     this.handlePosition.set(position);
                     this.handleValue.set(value);
                     this.#propagateChange?.(value);
@@ -166,20 +142,20 @@ export class SliderComponent implements OnInit, AfterViewInit, ControlValueAcces
             return;
         }
 
-        const containerRect = this.elementRef.nativeElement.getBoundingClientRect();
+        const containerRect = this.#hostElementRef.nativeElement.getBoundingClientRect();
         const handlePos =
             direction === "horizontal" ? event.clientX - containerRect.left : containerRect.bottom - event.clientY;
         const maxPos = direction === "horizontal" ? containerRect.width : containerRect.height;
         if (handlePos >= 0 && handlePos <= maxPos && handlePos !== this.handlePosition()) {
             const value = this.getValueFromPosition(handlePos);
-            if (!this.showTicks) {
-                this.zone.run(() => {
+            if (!this.showTicks()) {
+                this.#zone.run(() => {
                     this.handlePosition.set(handlePos);
                 });
             }
             if (value !== this.handleValue()) {
-                this.zone.run(() => {
-                    if (this.showTicks && this.handlePosition() !== handlePos) {
+                this.#zone.run(() => {
+                    if (this.showTicks() && this.handlePosition() !== handlePos) {
                         this.handlePosition.set(handlePos);
                     }
                     this.handleValue.set(value);
@@ -190,11 +166,11 @@ export class SliderComponent implements OnInit, AfterViewInit, ControlValueAcces
     }
 
     private setSubscription(): void {
-        this.zone.runOutsideAngular(() => {
+        this.#zone.runOutsideAngular(() => {
             fromEvent<MouseEvent>(this.sliderHandle.nativeElement, "mousedown")
                 .pipe(
                     takeUntilDestroyed(this.#destroyRef),
-                    filter(() => !this.disabled)
+                    filter(() => !this.disabled())
                 )
                 .subscribe(() => {
                     this.#mouseDown = true;
@@ -203,7 +179,7 @@ export class SliderComponent implements OnInit, AfterViewInit, ControlValueAcces
                         (event: MouseEvent) => {
                             if (this.#mouseDown) {
                                 this.#mouseMove = true;
-                                this.handleHandleMove(event, this.orientation);
+                                this.handleHandleMove(event, this.orientation());
                             }
                         }
                     );
@@ -213,20 +189,20 @@ export class SliderComponent implements OnInit, AfterViewInit, ControlValueAcces
                             this.#mouseDown = false;
                             this.#mouseMove = false;
                             moveSubscription.unsubscribe();
-                            this.zone.run(() => {
+                            this.#zone.run(() => {
                                 this.dragging.set(false);
                             });
                         });
                 });
 
-            fromEvent<MouseEvent>(this.elementRef.nativeElement, "click")
+            fromEvent<MouseEvent>(this.#hostElementRef.nativeElement, "click")
                 .pipe(
                     takeUntilDestroyed(this.#destroyRef),
                     tap(() => this.dragging.set(false)),
-                    filter(() => !this.disabled && !this.#mouseMove)
+                    filter(() => !this.disabled() && !this.#mouseMove)
                 )
                 .subscribe((event: MouseEvent) => {
-                    this.handleHandleMove(event, this.orientation);
+                    this.handleHandleMove(event, this.orientation());
                 });
 
             fromEvent<KeyboardEvent>(this.sliderHandle.nativeElement, "keydown")
@@ -236,17 +212,18 @@ export class SliderComponent implements OnInit, AfterViewInit, ControlValueAcces
                             event.key === "ArrowLeft" ||
                             event.key === "ArrowRight" ||
                             event.key === "ArrowUp" ||
-                            (event.key === "ArrowDown" && !this.disabled)
+                            (event.key === "ArrowDown" && !this.disabled())
                     ),
                     tap((event: KeyboardEvent) => {
                         event.stopPropagation();
-                        this.zone.run(() => {
+                        this.#zone.run(() => {
                             this.dragging.set(true);
                         });
                         const value = this.handleValue();
-                        const step = event.key === "ArrowLeft" || event.key === "ArrowDown" ? -this.step : this.step;
-                        const newValue = Math.max(this.min, Math.min(this.max, value + step));
-                        this.zone.run(() => {
+                        const step =
+                            event.key === "ArrowLeft" || event.key === "ArrowDown" ? -this.step() : this.step();
+                        const newValue = Math.max(this.min(), Math.min(this.max(), value + step));
+                        this.#zone.run(() => {
                             this.handleValue.set(newValue);
                             this.handlePosition.set(this.getPositionFromValue(newValue));
                             this.#propagateChange?.(newValue);
@@ -259,9 +236,7 @@ export class SliderComponent implements OnInit, AfterViewInit, ControlValueAcces
     }
 
     private findClosestTickElement(event: MouseEvent): HTMLSpanElement {
-        const elements = Array.from(
-            this.elementRef.nativeElement.querySelectorAll(".mona-slider-tick > span")
-        ) as HTMLSpanElement[];
+        const elements = Array.from(this.#hostElementRef.nativeElement.querySelectorAll(".mona-slider-tick > span"));
         let maxDistance = Number.MAX_VALUE;
         let index = 0;
         for (const [ex, element] of elements.entries()) {
@@ -272,21 +247,6 @@ export class SliderComponent implements OnInit, AfterViewInit, ControlValueAcces
                 index = ex;
             }
         }
-        return elements[index];
-    }
-
-    private prepareTicks(): void {
-        let index = 0;
-        let value = this.min;
-        this.ticks = [];
-        while (value < this.max) {
-            this.ticks.push({ index, value });
-            value += this.step;
-            index++;
-        }
-        this.ticks.push({ index, value: Math.min(value + this.step, this.max) });
-        if (this.orientation === "vertical") {
-            this.ticks.reverse();
-        }
+        return elements[index] as HTMLSpanElement;
     }
 }
