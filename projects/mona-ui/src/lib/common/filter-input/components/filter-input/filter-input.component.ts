45af0a83d08fccc3b3912d615f039394996977d8
Nanahoshi <nanahoshilily@outlook.com>
2024-02-19 02:22:19 +0300

    refactor: filter input style updates

--- projects/mona-ui/src/lib/common/filter-input/components/filter-input/filter-input.component.ts
+++ projects/mona-ui/src/lib/common/filter-input/components/filter-input/filter-input.component.ts
@@ -1,12 +1,15 @@
 import {
     afterNextRender,
+    ChangeDetectionStrategy,
     Component,
     DestroyRef,
     ElementRef,
     EventEmitter,
     inject,
     Injector,
+    input,
     Input,
+    InputSignal,
     OnInit,
     Output,
     signal,
@@ -24,20 +27,24 @@ import { FilterChangeEvent } from "../../models/FilterChangeEvent";
 @Component({
     selector: "mona-filter-input",
     standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [TextBoxComponent, FormsModule, TextBoxPrefixTemplateDirective, FaIconComponent],
     templateUrl: "./filter-input.component.html",
-    styleUrl: "./filter-input.component.scss"
+    styleUrl: "./filter-input.component.scss",
+    host: {
+        class: "mona-filter-input"
+    }
 })
 export class FilterInputComponent implements OnInit {
     readonly #destroyRef: DestroyRef = inject(DestroyRef);
+    readonly #hostElementRef: ElementRef<HTMLElement> = inject(ElementRef);
     readonly #injector: Injector = inject(Injector);
     protected readonly filter$: Subject<string> = new Subject<string>();
     protected readonly filterIcon = faSearch;
-    protected readonly filterPlaceholder: WritableSignal<string> = signal("");
     protected readonly filterText: WritableSignal<string> = signal("");
 
-    @Input()
-    public debounce: number = 0;
+    public debounce: InputSignal<number> = input(0);
+    public placeholder: InputSignal<string> = input("");
 
     @Input()
     public set filter(value: string) {
@@ -47,19 +54,12 @@ export class FilterInputComponent implements OnInit {
     @Output()
     public filterChange: EventEmitter<FilterChangeEvent> = new EventEmitter<FilterChangeEvent>();
 
-    @Input()
-    public set placeholder(value: string) {
-        this.filterPlaceholder.set(value);
-    }
-
-    public constructor(private readonly elementRef: ElementRef<HTMLElement>) {}
-
     public ngOnInit(): void {
         this.setSubscriptions();
     }
 
     private setSubscriptions(): void {
-        this.filter$.pipe(debounceTime(this.debounce), takeUntilDestroyed(this.#destroyRef)).subscribe(value => {
+        this.filter$.pipe(debounceTime(this.debounce()), takeUntilDestroyed(this.#destroyRef)).subscribe(value => {
             const event = new FilterChangeEvent(value);
             this.filterChange.emit(event);
             if (!event.isDefaultPrevented()) {
@@ -68,7 +68,10 @@ export class FilterInputComponent implements OnInit {
         });
         afterNextRender(
             () => {
-                fromEvent<KeyboardEvent>(this.elementRef.nativeElement.querySelector("input") as HTMLElement, "keydown")
+                fromEvent<KeyboardEvent>(
+                    this.#hostElementRef.nativeElement.querySelector("input") as HTMLElement,
+                    "keydown"
+                )
                     .pipe(takeUntilDestroyed(this.#destroyRef))
                     .subscribe((event: KeyboardEvent) => {
                         if (event.key === "ArrowDown" || event.key === "ArrowUp") {
