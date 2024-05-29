f65ec2e51263420a95541e09fe2acd207b575668
Nanahoshi <nanahoshilily@outlook.com>
2024-05-29 17:58:52 +0300

    Editor font color picker ui improvements

--- projects/mona-ui/src/lib/editor/components/editor-font-color/editor-font-color.component.ts
+++ projects/mona-ui/src/lib/editor/components/editor-font-color/editor-font-color.component.ts
@@ -1,23 +1,29 @@
 import { ChangeDetectionStrategy, Component, computed, inject } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { ColorPickerComponent } from "../../../inputs/color-picker/color-picker.component";
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { faFont } from "@fortawesome/free-solid-svg-icons";
+import { ColorPickerComponent } from "../../../inputs/color-picker/components/color-picker/color-picker.component";
+import { ColorPickerValueTemplateDirective } from "../../../inputs/color-picker/directives/color-picker-value-template.directive";
+import { ColorService } from "../../../inputs/services/color.service";
 import { EditorService } from "../../services/editor.service";
 
 @Component({
     selector: "mona-editor-font-color",
     standalone: true,
-    imports: [ColorPickerComponent, FormsModule],
+    imports: [ColorPickerComponent, FormsModule, ColorPickerValueTemplateDirective, FaIconComponent],
     templateUrl: "./editor-font-color.component.html",
     styleUrl: "./editor-font-color.component.scss",
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EditorFontColorComponent {
     readonly #editorService: EditorService = inject(EditorService);
+    protected readonly fontIcon = faFont;
     protected readonly selectedColor = computed(() => {
         this.#editorService.state();
         const attributes = this.#editorService.editor.getAttributes("textStyle");
-        return attributes["color"];
+        return ColorService.getHtmlColorCode(attributes["color"]);
     });
+
     public onColorChange(color: string): void {
         this.#editorService.editor.chain().focus().setColor(color).run();
     }
